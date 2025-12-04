05c701fec9898374d40fb29b83595e06ea4057eb
GUALLAL <guallalaimad@gmail.com>
2025-12-04 05:09:09 +0100

    Test CI/CD: add comment to dim_customer

--- dbt-etl/models/transform/dim_customer.sql
+++ dbt-etl/models/transform/dim_customer.sql
@@ -1,5 +1,6 @@
 -- dim_customer.sql
 -- Create the dimension table
+-- Test the CI/CD pipeline when pushing this update
 WITH customer_cte AS (
   SELECT DISTINCT
     {{ dbt_utils.generate_surrogate_key(['CustomerID', 'Country']) }} as customer_id,
